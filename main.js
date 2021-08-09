1327daa05ff1d364342a3cbdf08ff97762bc3d21
LySeriuos <drungilas.p@gmail.com>
2021-08-09 17:43:14 +0000

    Implement pagination

--- main.js
+++ main.js
@@ -1,50 +1,61 @@
-const baseURL = "https://ci-swapi.herokuapp.com/api/";
-
-function getData(type, cb) { 
-
+function getData(url, cb) {
     var xhr = new XMLHttpRequest();
 
-xhr.open("GET", baseURL + type + "/");
-xhr.send();
+    xhr.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            cb(JSON.parse(this.responseText));
+        }
+    };
 
-xhr.onreadystatechange = function(){
-   
-  if (this.readyState == 4 && this.status == 200) {  
-           cb(JSON.parse(this.responseText));
-  }  
-};
+    xhr.open("GET", url);
+    xhr.send();
 }
 
-function getTableHeaders (obj) {
+function getTableHeaders(obj) {
     var tableHeaders = [];
 
-    Object.keys(obj).forEach(function(key){
+    Object.keys(obj).forEach(function(key) {
         tableHeaders.push(`<td>${key}</td>`);
     });
+
     return `<tr>${tableHeaders}</tr>`;
 }
 
-function writeToDocument(type) {
+function generatePaginationButtons(next, prev) {
+    if (next && prev) {
+        return `<button onclick="writeToDocument('${prev}')">Previous</button>
+                <button onclick="writeToDocument('${next}')">Next</button>`;
+    } else if (next && !prev) {
+        return `<button onclick="writeToDocument('${next}')">Next</button>`;
+    } else if (!next && prev) {
+        return `<button onclick="writeToDocument('${prev}')">Previous</button>`;
+    }
+}
+
+function writeToDocument(url) {
     var tableRows = [];
     var el = document.getElementById("data");
-    //el.innerHTML = ""; /*So that will clear it every time the button is clicked.*/
-   
-    getData(type, function(data) {
+
+    getData(url, function(data) {
+        var pagination = "";
+
+        if (data.next || data.previous) {
+            pagination = generatePaginationButtons(data.next, data.previous);
+        }
         data = data.results;
-        /*document.getElementById("data").innerHTML = data.results; /*to reach data from the array named "results"*/
-        var tableHeaders =  getTableHeaders(data[0]);
+        var tableHeaders = getTableHeaders(data[0]);
 
         data.forEach(function(item) {
-             var dataRow = [];
+            var dataRow = [];
 
-             Object.keys(item).forEach(function(key){
-                 var rowData = item[key].toString();
-                 var truncatedData = rowData.substring(0, 15)
-                dataRow.push(`<td>${truncatedData}</td>`); //And what we do is we have {$item}, and then we pass in [key] as the index, so this will actually get us the data that's in each individual key. Rather than just the key name itself, we'll get the value.
-             });
-          tableRows.push(`<tr>${dataRow}</tr>`);
+            Object.keys(item).forEach(function(key) {
+                var rowData = item[key].toString();
+                var truncatedData = rowData.substring(0, 15);
+                dataRow.push(`<td>${truncatedData}</td>`);
             });
-            el.innerHTML = `<table>${tableHeaders}${tableRows}</table>`;
-    
+            tableRows.push(`<tr>${dataRow}</tr>`);
+        });
+
+        el.innerHTML = `<table>${tableHeaders}${tableRows}</table>${pagination}`;
     });
 }
